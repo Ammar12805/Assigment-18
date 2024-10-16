cca117b95bc6f2543e8a7175d3c52aa68bac2abf
Ammar Ahmed <ammarahmed12805@gmail.com>
2024-10-16 19:03:33 +0500

    Uploading two Files

---
+++ app.js
@@ -0,0 +1,61 @@
+// Q1
+
+
+// var currentData = new Date();
+// console.log(currentData);
+
+
+// Q2
+
+
+// var month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+
+// var date = new Date();
+// console.log(month[date.getMonth()]);
+
+
+
+
+// Q3
+
+
+// var day = prompt(`Enter a Day`)
+
+// var d = day.slice(0,1).toUpperCase;
+// alert(day.slice(0,3)); 
+
+
+// Q4
+
+
+// var day = prompt(`Enter a Day`);
+
+// var d = day.slice(0,1).toUpperCase();
+
+// if(day === `Saturday`){
+//     alert(`its Fun day`)
+// }else if (day === `sunday`){
+//     alert(`its Fun day`)
+
+// }else{
+//     alert(`its Working day`)
+
+// }
+
+
+// Q5
+
+
+// var date = +prompt(`Enter date of Month`);
+
+// if(date <=15){
+//     alert(`First Fifteen Day of Month`);
+// }else{
+//     alert(`Last days of Month`);
+// };
+
+
+
+
+
+
